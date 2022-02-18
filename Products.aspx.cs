0cbc9c283d67ae1a94cc14bf432f1184a511994b
Ismayilov Raqif <raqif.ismayilov@gmail.com>
2022-02-18 11:26:41 +0400

    aaa

--- Products.aspx.cs
+++ Products.aspx.cs
@@ -12,7 +12,6 @@ public partial class Products : System.Web.UI.Page
     Methods _db = new Methods();
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Kele
         _loadGridFromDb();
         if (IsPostBack) return;
     }
