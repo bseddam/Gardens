3ac9e3bdfd17de32970e36cc04a9b91d798d1ac0
seddam bayramov <bseddam@gmail.com>
2022-02-22 10:17:23 +0400

    dd

--- WeatherCondition.aspx.cs
+++ WeatherCondition.aspx.cs
@@ -1,14 +1,98 @@
-﻿using System;
+﻿using DevExpress.Web;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class WeatherCondition : System.Web.UI.Page
 {
+    Methods _db = new Methods();
     protected void Page_Load(object sender, EventArgs e)
     {
+        _loadGridFromDb();
+        if (IsPostBack) return;
+    }
+    void ClearComponents()
+    {
+        txtweathercondition.Text = "";
+        txtcoefficient.Text = "";
+        lblPopError.Text = "";
+    }
+    void _loadGridFromDb()
+    {
+        DataTable dt = _db.GetWeatherCondition();
+        if (dt != null)
+        {
+            Grid.SettingsPager.Summary.Text = "Cari səhifə: {0}, Ümumi səhifələrin sayı: {1}, Tapılmış məlumatların sayı: {2}";
+            Grid.DataSource = dt;
+            Grid.DataBind();
+        }
+    }
 
+
+    protected void lnkEdit_Click(object sender, EventArgs e)
+    {
+
+        int id = (sender as LinkButton).CommandArgument.ToParseInt();
+        DataTable dt = _db.GetWeatherConditionByID(id: id);
+
+        txtweathercondition.Text = dt.Rows[0]["WeatherConditionName"].ToParseStr();
+
+        btnSave.CommandName = "update";
+        btnSave.CommandArgument = id.ToString();
+        popupEdit.ShowOnPageLoad = true;
+    }
+    protected void lnkDelete_Click(object sender, EventArgs e)
+    {
+        int _id = (sender as LinkButton).CommandArgument.ToParseInt();
+        Types.ProsesType val = _db.DeleteWeatherCondition(id: _id);
+        _loadGridFromDb();
+    }
+    protected void LnkPnlMenu_Click(object sender, EventArgs e)
+    {
+        ClearComponents();
+        LinkButton btn = sender as LinkButton;
+        switch (btn.CommandArgument)
+        {
+            case "add":
+                btnSave.CommandName = "insert";
+                popupEdit.ShowOnPageLoad = true;
+                break;
+        }
+    }
+    protected void btntesdiq_Click(object sender, EventArgs e)
+    {
+        lblPopError.Text = "";
+        Types.ProsesType val = Types.ProsesType.Error;
+        if (btnSave.CommandName == "insert")
+        {
+            val = _db.WeatherConditionInsert(
+                WeatherConditionName: txtweathercondition.Text.ToParseStr(),
+                Coefficient: txtcoefficient.Text.ToParseStr());
+        }
+        else
+        {
+            val = _db.WeatherConditionUpdate(
+                WeatherConditionID: btnSave.CommandArgument.ToParseInt(),
+                WeatherConditionName: txtweathercondition.Text.ToParseStr(),
+                Coefficient: txtcoefficient.Text.ToParseStr());
+        }
+
+        if (val == Types.ProsesType.Error)
+        {
+            lblPopError.Text = "XƏTA! Yadda saxlamaq mümkün olmadı.";
+            return;
+        }
+
+        _loadGridFromDb();
+        popupEdit.ShowOnPageLoad = false;
+    }
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        popupEdit.ShowOnPageLoad = false;
     }
 }
